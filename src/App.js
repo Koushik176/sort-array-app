e34ed691b246a940757cb5873d8530aa42db5c2f
Koushik <koushikashok176@gmail.com>
2023-07-23 18:26:52 +0530

    ascending and descending orderlist dynamic

--- src/App.js
+++ src/App.js
@@ -7,16 +7,37 @@ import Button from "./Components/UI/Button/Button";
 
 function App() {
   const [listTitle, setListTitle] = useState("My List");
+  const [listOrder, setListOrder] = useState("ascending");
+  const [buttonText, setButtonText] = useState("change to descending order");
 
   const changeTitleHandler = useCallback(() => {
     setListTitle("New Title");
   }, []);
 
+  const changeOrderHandler = useCallback(() => {
+    setButtonText((prevText) => {
+      if(prevText === "change to descending order") {
+        return "change to ascending order";
+      }else {
+        return "change to descending order";
+      }
+    })
+    setListOrder((prevOrder) => {
+      if(prevOrder === "ascending") {
+        return "descending";
+      }
+      else{
+        return "ascending";
+      }
+    });
+  }, []);
+
   const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
   return (
     <div className="app">
-      <DemoList title={listTitle} items={listItems} />
+      <DemoList title={listTitle} items={listItems} order={listOrder} />
       <Button onClick={changeTitleHandler}>Change List Title</Button>
+      <Button onClick={changeOrderHandler}>{buttonText}</Button>
     </div>
   );
 }
--- src/Components/Demo/DemoList.js
+++ src/Components/Demo/DemoList.js
@@ -4,19 +4,34 @@ import classes from './DemoList.module.css';
 
 const DemoList = (props) => {
   const { items } = props;
+  const order = props.order;
 
-  const sortedList = useMemo(() => {
-    console.log('Items sorted');
-    return items.sort((a, b) => a - b);
-  }, [items]); 
-  console.log('DemoList RUNNING');
+  const descendingSortedList = useMemo(() => {
+    if(order === "descending"){
+      console.log("descending");
+      return items.sort((a, b) => b - a);
+    }
+  }, [order, items]);
+  
+  const ascendingSortedList = useMemo(() => {
+    if(order === "ascending") {
+      console.log("ascending");
+      return items.sort((a, b) => a - b);
+    }
+  }, [order,items]);
+
+  //console.log('DemoList RUNNING');
+
 
   return (
     <div className={classes.list}>
       <h2>{props.title}</h2>
       <ul>
-        {sortedList.map((item) => (
+        {order === "ascending" && ascendingSortedList.map((item) => (
           <li key={item}>{item}</li>
+        ))}
+        {order === "descending" && descendingSortedList.map((item) => (
+            <li key={item}>{item}</li>
         ))}
       </ul>
     </div>
